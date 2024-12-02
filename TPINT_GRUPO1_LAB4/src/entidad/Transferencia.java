d5737bae019ae56a471039f1d40a76c86f98226e
Alejo Chavez <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-12-02 12:30:47 -0300

    Clase Transferencia

--- TPINT_GRUPO1_LAB4/src/entidad/Transferencia.java
+++ TPINT_GRUPO1_LAB4/src/entidad/Transferencia.java
@@ -6,4 +6,29 @@ public class Transferencia {
 	private Cuenta cuentaOrigen;
 	private Cuenta cuentaDestino;
 	private BigDecimal monto;
+	
+	
+	public Transferencia() {
+		super();
+	}
+	public Cuenta getCuentaOrigen() {
+		return cuentaOrigen;
+	}
+	public void setCuentaOrigen(Cuenta cuentaOrigen) {
+		this.cuentaOrigen = cuentaOrigen;
+	}
+	public Cuenta getCuentaDestino() {
+		return cuentaDestino;
+	}
+	public void setCuentaDestino(Cuenta cuentaDestino) {
+		this.cuentaDestino = cuentaDestino;
+	}
+	public BigDecimal getMonto() {
+		return monto;
+	}
+	public void setMonto(BigDecimal monto) {
+		this.monto = monto;
+	}
+	
+	
 }
